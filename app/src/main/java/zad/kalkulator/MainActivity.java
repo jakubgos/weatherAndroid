ce32fdc36c95caa9a81ac508e17179f3950d3842
Your Name <you@example.com>
2016-10-20 22:28:27 +0200

    Relative layout support

--- app/src/main/java/zad/kalkulator/MainActivity.java
+++ app/src/main/java/zad/kalkulator/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -23,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void changeToRelative(View view) {
+        Intent intent = new Intent(this, RelativeCaclActivity.class);
+        startActivity(intent);
+    }
+
+    public void printAbout(View view) {
+        Toast.makeText(getApplicationContext(), "Author: JakubJGos", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void doExit(View view) {
+        finish();
+        System.exit(0);
     }
 }
--- app/src/main/java/zad/kalkulator/RelativeCaclActivity.java
+++ app/src/main/java/zad/kalkulator/RelativeCaclActivity.java
@@ -1,20 +1,19 @@
 package zad.kalkulator;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LinearCalcActivity extends AppCompatActivity {
+public class RelativeCaclActivity extends AppCompatActivity {
     TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear_calc);
+        setContentView(R.layout.activity_relative_cacl);
         t = (TextView) findViewById(R.id.txtScreen);
     }
-
     public void buttonUse(View view) {
 
         t.setText(t.getText() + (String) view.getTag());
